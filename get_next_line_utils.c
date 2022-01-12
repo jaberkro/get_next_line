2b97b08daf42353ef673be2197ccd2c3f1c48db7
Jaberkro <Jorien@MacBook-Air-van-Jorien-2.local>
2022-01-12 12:41:09 +0100

    moved functions from get_next_line.c to this file

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,34 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/08 11:48:31 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/01/08 12:06:53 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/01/12 12:32:31 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+int	gnl_strlen(char *input)
+{
+	int	count;
+
+	count = 0;
+	if (!input)
+		return (0);
+	while (input && input[count])
+		count++;
+	return (count);
+}
+
+int	newline(char *buf, int found)
+{
+	int	i;
+
+	i = 0;
+	while (i < found)
+	{
+		if (buf[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
