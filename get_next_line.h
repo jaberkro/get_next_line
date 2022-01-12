fe864b7a2e132f683ca03e42c3053a3fc061302a
Jaberkro <Jorien@MacBook-Air-van-Jorien-2.local>
2022-01-12 12:41:59 +0100

    added 2 functions to headerfile

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/08 11:48:52 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/01/08 16:50:18 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/01/12 12:33:13 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,7 @@
 # include <stdio.h>
 
 char    *get_next_line(int fd);
+int     gnl_strlen(char *input);
+int     newline(char *buf, int found);
 
 #endif
