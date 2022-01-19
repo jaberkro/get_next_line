aa488e79f9ae9cbd4f4e49a15261500b83186f69
Jorien Aberkrom <jaberkro@student.codam.nl>
2022-01-19 21:37:17 +0100

    optimized

--- get_next_line.h
+++ get_next_line.h
@@ -5,18 +5,19 @@
 /*                                                     +:+                    */
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/01/08 11:48:52 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/01/12 12:57:55 by jaberkro      ########   odam.nl         */
+/*   Created: 2022/01/19 21:12:22 by jaberkro      #+#    #+#                 */
+/*   Updated: 2022/01/19 21:14:58 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <unistd.h>
+# include <fcntl.h>
 # include <stdlib.h>
 
 char	*get_next_line(int fd);
 int		gnl_strlen(char *input);
-int		newline(char *buf, int found);
+int		gnl_strchr_nl(char *input);
 
 #endif
